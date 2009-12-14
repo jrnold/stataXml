428c8413bcc31fe984f38b0ae6eae5da945ac5c5
Jeffrey.Arnold <jeffrey.arnold@gmail.com>
2009-12-14 00:45:21 -0500

    remove load.R

--- load.R
+++
@@ -1,7 +0,0 @@
-library(plyr)
-library(stataXml, warn.conflicts = FALSE)
-
-frame_files <- compact(llply(sys.frames(), function(x) x$ofile))
-PATH <- dirname(frame_files[[length(frame_files)]])
-
-lapply(dir(file.path(PATH, "R"), full.name=TRUE), source)
